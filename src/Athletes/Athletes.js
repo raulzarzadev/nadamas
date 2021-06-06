a439d568d0e73820ea72af2398f5d9b15d93983b
Raul Zarza <raulzarza.dev@gmail.com>
2021-06-06 03:58:50 -0600

    ahora modal emergency works

--- src/Athletes/Athletes.js
+++ src/Athletes/Athletes.js
@@ -3,7 +3,8 @@ import Button from '../Button'
 import s from './styles.module.css'
 import { getAthletes } from 'firebase/client'
 import { useRouter } from 'next/router'
-import { ContactIcon, EditIcon, EmergencyIcon } from '../utils/Icons'
+import { ContactIcon, EditIcon, EmergencyIcon, CallIcon } from '../utils/Icons'
+import Modal from '../Modal'
 
 export default function Athletes() {
   const router = useRouter()
@@ -17,8 +18,10 @@ export default function Athletes() {
   const handleClickAthlete = (id) => {
     router.push(`/atletas/${id}`)
   }
-  const handleOpenEmergencyCall = (contact) => {
-    console.log('open emergency call', contact)
+
+  const [openEmergencyModal, setOpenEmergencyModal] = useState(false)
+  const handleOpenEmergencyCall = () => {
+    setOpenEmergencyModal(!openEmergencyModal)
   }
   return (
     <div className={s.athletes}>
@@ -29,24 +32,14 @@ export default function Athletes() {
       </Button>
       <div>
         {athletes.map(
-          ({ id, name, mobile, emerTitle, emerName, emerMobile }) => (
+          ({ id, name, lastName, mobile, emerTitle, emerName, emerMobile }) => (
             <div className={s.athlete_row}>
               <div className={s.athlete} key={id}>
-                {name}
+                {`${name} ${lastName[0]}.`}
               </div>
               <div className={s.athlete_action}>
-                <Button icon nextLink href={`/atletas/${id}`}>
-                  <EditIcon />
-                </Button>
-              </div>
-              <div className={s.athlete_action}>
-                <Button
-                  icon
-                  onClick={() =>
-                    handleOpenEmergencyCall({ emerTitle, emerName, emerMobile })
-                  }
-                >
-                  <EmergencyIcon />
+                <Button icon onClick={handleOpenEmergencyCall}>
+                  <EmergencyIcon style={{ color: 'red' }} />
                 </Button>
               </div>
               <div className={s.athlete_action}>
@@ -54,10 +47,41 @@ export default function Athletes() {
                   <ContactIcon />
                 </Button>
               </div>
+              <div className={s.athlete_action}>
+                <Button icon nextLink href={`/atletas/${id}`}>
+                  <EditIcon />
+                </Button>
+              </div>
+              <EmergencyCallModal
+                handleOpen={handleOpenEmergencyCall}
+                open={openEmergencyModal}
+                contact={{ emerTitle, emerName, emerMobile, name }}
+              />
             </div>
           )
         )}
       </div>
     </div>
+  )
+}
+
+const EmergencyCallModal = ({ contact, handleOpen, open }) => {
+  const { emerTitle, emerName, emerMobile, name } = contact
+  return (
+    <Modal handleOpen={handleOpen} open={open} title="Llamada de emergencia">
+      <div>
+        {`¿Segura quieres llamar a `}
+        <strong>{emerName}</strong>
+        {`, `}
+        <>{emerTitle}</>
+        {` de `}
+        <strong>{name}</strong>
+        {`? `}
+        <Button my="md" danger nextLink href={`tel:+52${emerMobile}`}>
+          <CallIcon />
+          {` Llamar`}
+        </Button>
+      </div>
+    </Modal>
   )
 }
--- src/utils/Icons.js
+++ src/utils/Icons.js
@@ -6,6 +6,8 @@ import { RiHeartPulseLine } from 'react-icons/ri'
 import { RiTimeFill } from 'react-icons/ri'
 import { RiWhatsappFill } from 'react-icons/ri'
 import { RiArrowLeftSLine } from 'react-icons/ri'
+import { RiCloseFill } from 'react-icons/ri'
+import { RiCustomerService2Fill } from 'react-icons/ri'
 
 const size = '1.8rem'
 export const EditIcon = (props) => <RiEditFill size={size} {...props} />
@@ -16,3 +18,5 @@ export const ScheduleIcon = (props) => <RiTimeFill size={size} {...props} />
 export const ContactIcon = (props) => <RiWhatsappFill size={size} {...props} />
 
 export const BackIcon = (props) => <RiArrowLeftSLine size={size} {...props} />
+export const CloseIcon = (props) => <RiCloseFill size={size} {...props} />
+export const CallIcon = (props) => <RiCustomerService2Fill size={size} {...props} />
