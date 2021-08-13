50541dab96096707c57cb9dd9aa9ad100ad8f487
Raul Zarza <raulzarza.dev@gmail.com>
2021-08-13 11:22:38 -0600

    new link into navbar

--- src/layouts/MainLayout/Navbar.js
+++ src/layouts/MainLayout/Navbar.js
@@ -21,6 +21,10 @@ export default function Navbar() {
       label: 'Atletas'
     },
     {
+      href: '/athletes/new',
+      label: 'Nuevo atleta'
+    },
+    {
       href: '/signout',
       label: 'Salir'
     }
