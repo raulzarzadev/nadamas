34ca95aa9c0c6330da38bdaace38546dc976c4e4
Raul Zarza <raulzarza.dev@gmail.com>
2021-11-08 11:43:12 -0700

    update auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,7 +4,6 @@ import {
   loginWithGoogle,
   onAuthStateChanged
 } from '@/firebase/client'
-import { getSchedules } from '@/firebase/schedules'
 import { useRouter } from 'next/router'
 const AuthContext = createContext()
 
@@ -12,36 +11,11 @@ export function AuthProvider({ children }) {
   const router = useRouter()
   const [user, setUser] = useState()
 
-  const ENVIROMENT = process.env.NEXT_PUBLIC_ENVIROMENT
-  console.log('ENVIROMENT', ENVIROMENT)
-
   useEffect(() => {
-    if (ENVIROMENT === 'DEV') {
-      setUser({
-        email: 'raul3arza@gmail.com',
-        id: 'osdRhDFpycZCa2e6o7QdEsDRbzw2',
-        image: '',
-        joinedAt: 324234,
-        name: 'Zarza Dev'
-      })
-    } else if (!user) {
       onAuthStateChanged(setUser)
-    }
   }, [])
 
-  /*   
-  const [userSchedule, setUserSchedule] = useState({})
-  useEffect(() => {
-    if (user) {
-      getSchedules(user?.id)
-        .then(( res ) => {
-          console.log(`res`, res)
-          setUserSchedule(res[0].schedule)})
-        .catch((err) => console.log('err', err))
-    }
-  }, [user]) */
-
-  // confirm if user have isActive
+ 
 
   const signOut = () => {
     firebaseLogout()
@@ -60,7 +34,7 @@ export function AuthProvider({ children }) {
   }
 
   return (
-    <AuthContext.Provider value={{ user, googleLogin, signOut }}>
+    <AuthContext.Provider value={{ user,  googleLogin, signOut }}>
       {children}
     </AuthContext.Provider>
   )
