517e29ad9f703ff9e4dd0f716315873367be39ca
Raul Zarza <raulzarza.dev@gmail.com>
2021-11-12 10:57:44 -0700

    cambia boton login por redirect

--- src/components/MainLayout/PageErrors/MustBeAuthenticated.js
+++ src/components/MainLayout/PageErrors/MustBeAuthenticated.js
@@ -1,11 +1,18 @@
 import Info from '@comps/Alerts/Info'
 import SocialMediaLogin from '@comps/AuthCard/SocialMediaLogin'
+import Button from '@comps/inputs/Button'
+import { useRouter } from 'next/router'
 
 export default function MustBeAuthenticated() {
+  const router = useRouter()
   return (
     <div className="max-w-md mx-auto pt-6">
-        <Info text="Debe estar autenticado para poder ver esta pagina" fullWidth/>
-      <SocialMediaLogin />
+      <Info
+        text="Debe estar autenticado para poder ver esta pagina"
+        fullWidth
+      />
+      <Button label="Ingresar" onClick={() => router.push('/signin')} />
+      {/*  <SocialMediaLogin /> */}
     </div>
   )
 }
