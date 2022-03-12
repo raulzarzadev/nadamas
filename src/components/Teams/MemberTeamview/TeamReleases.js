0d9e7d1261ae661197311bbd61ce9b036c1778e5
Raul Zarza <raulzarza.dev@gmail.com>
2022-03-12 00:12:39 -0600

    adding seciton in team view

---
+++ src/components/Teams/MemberTeamview/TeamReleases.js
@@ -0,0 +1,4 @@
+export default function TeamReleases({ team }) {
+  console.log(team)
+  return <div className="">Aun no hay comunicados</div>
+}
--- src/components/Teams/MemberTeamview/index.js
+++ src/components/Teams/MemberTeamview/index.js
@@ -1,15 +1,24 @@
-import MemberRow from '../MemberRow'
+import Section from '@comps/Section'
 import TeamMembers from '../TeamMembers'
+import TeamReleases from './TeamReleases'
 
 export default function MemberTeamView({ team }) {
   return (
     <div className="">
-      <TeamMembers
-        coachView={false}
-        teamCoaches={team?.coaches}
-        teamId={team.id}
-        members={team?.athletes}
-      />
+      <Section title={'Comunicados'} indent={false} open>
+        <TeamReleases team={team} />
+      </Section>
+      <Section title="Miembros" indent={false} open>
+        <TeamMembers
+          coachView={false}
+          teamCoaches={team?.coaches}
+          teamId={team.id}
+          members={team?.athletes}
+        />
+      </Section>
+      <Section title="Mis cuotoas">
+        Esta información aun no esta disponible
+      </Section>
     </div>
   )
 }
