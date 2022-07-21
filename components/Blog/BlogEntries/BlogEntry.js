2092b56831d8660c92573c6eb97b9fd5e5ee7a01
zasara <raulzarza@gmail.com>
2022-07-21 15:23:32 -0600

    change blocked entry div

--- components/Blog/BlogEntries/BlogEntry.js
+++ components/Blog/BlogEntries/BlogEntry.js
@@ -65,11 +65,6 @@ const BlogEntry = ({ entry, blocked = true }) => {
 
       <div className='flex items-center  '>
         
-        {blocked &&
-          <div className="absolute h-60 w-full bottom-0  bg-gradient-to-t from-black to-transparent flex justify-center items-end pb-6 " >
-            <button className='text-white' onClick={() => router.push(`${ROUTES.BLOG.href}/${entry.id}`)}>Click para ver articulo</button>
-          </div>
-        }
 
         <SideButtons
           entryId={entry.id}
@@ -83,6 +78,11 @@ const BlogEntry = ({ entry, blocked = true }) => {
           <MarkdownEntry content={entry?.content} />
         </div>
       </div>
+        {blocked &&
+          <div className="absolute h-60 w-full bottom-0  bg-gradient-to-t from-black to-transparent flex justify-center items-end pb-6 " >
+            <button className='text-white' onClick={() => router.push(`${ROUTES.BLOG.href}/${entry.id}`)}>Click para ver articulo</button>
+          </div>
+        }
       <ButtonAdd
         onClick={() => router.push(`${ROUTES.BLOG.href}/new`)}
       />
