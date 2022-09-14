d383997a5bb2f56728175d0701ae4cfafeac5897
raulzarza <raulzarza.dev@gmail.com>
2022-09-14 17:16:17 -0600

    fix prose tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,92 +1,94 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
 // Let's create a plugin that adds utilities!
 const capitalizeFirst = plugin(function ({ addUtilities }) {
   const newUtilities = {
-    '.capitalize-first:first-letter': {
-      textTransform: 'uppercase'
-    }
-  }
-  addUtilities(newUtilities, ['responsive', 'hover'])
-})
+    ".capitalize-first:first-letter": {
+      textTransform: "uppercase",
+    },
+  };
+  addUtilities(newUtilities, ["responsive", "hover"]);
+});
 const checkedSiblingPlugin = plugin(function ({ addVariant, e }) {
-  addVariant('checked-sibiling', ({ container }) => {
+  addVariant("checked-sibiling", ({ container }) => {
     container.walkRules((rule) => {
-      rule.selector = `:checked ~ .checked-sibiling\\:${rule.selector.slice(1)}`
-    })
-  })
-})
+      rule.selector = `:checked ~ .checked-sibiling\\:${rule.selector.slice(
+        1
+      )}`;
+    });
+  });
+});
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
     // './src/components/**/*.{js}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkTheme: 'dark',
+  darkTheme: "dark",
   daisyui: {
-    themes: ['dark', 'light', 'cupcake']
+    themes: ["dark", "light", "cupcake"],
   },
 
   variants: {
     extend: {
-      display: ['group-hover'],
-      opacity: ['disabled'],
-      backgroundColor: ['checked', 'checked-sibiling'],
-      borderColor: ['checked'],
-      translate: ['active', 'checked-sibiling']
-    }
+      display: ["group-hover"],
+      opacity: ["disabled"],
+      backgroundColor: ["checked", "checked-sibiling"],
+      borderColor: ["checked"],
+      translate: ["active", "checked-sibiling"],
+    },
   },
   plugins: [
+    require("@tailwindcss/typography"),
     checkedSiblingPlugin,
-    require('daisyui'),
+    require("daisyui"),
     capitalizeFirst,
-    require('@tailwindcss/typography')
-  ]
-}
+  ],
+};
 
 const theme = {
   colors: {
     primary: {
-      DEFAULT: '#00b7fa',
-      dark: '#094f68',
-      light: '#22c4ff'
+      DEFAULT: "#00b7fa",
+      dark: "#094f68",
+      light: "#22c4ff",
     },
     secondary: {
-      light: '#3093e4',
-      DEFAULT: '#ae83e6',
-      dark: '#173e4d'
+      light: "#3093e4",
+      DEFAULT: "#ae83e6",
+      dark: "#173e4d",
     },
     success: {
-      DEFAULT: '#81e686'
+      DEFAULT: "#81e686",
     },
     danger: {
-      DEFAULT: '#ff443b'
+      DEFAULT: "#ff443b",
     },
     warning: {
-      DEFAULT: '#ffe600'
+      DEFAULT: "#ffe600",
     },
     info: {
-      DEFAULT: '#daf3fc'
+      DEFAULT: "#daf3fc",
     },
     dark: {
-      DEFAULT: '#292d33'
+      DEFAULT: "#292d33",
     },
     light: {
-      DEFAULT: '#bfc8d4'
+      DEFAULT: "#bfc8d4",
     },
     white: {
-      DEFAULT: '#e9edf3'
+      DEFAULT: "#e9edf3",
     },
     black: {
-      DEFAULT: '#242525'
+      DEFAULT: "#242525",
     },
-    transparent: 'rgb(0,0,0,0)'
+    transparent: "rgb(0,0,0,0)",
   },
   fontFamily: {
-    sans: ['Graphik', 'sans-serif'],
-    serif: ['Merriweather', 'serif']
-  }
-}
+    sans: ["Graphik", "sans-serif"],
+    serif: ["Merriweather", "serif"],
+  },
+};
